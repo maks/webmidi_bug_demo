529ef691cc9561e2a8d2905bd62e634c5dba2b5a
Maksim Lin <maks@manichord.com>
2022-02-09 10:46:47 +1100

    add code to show js binding forEach not working

--- web/main.dart
+++ web/main.dart
@@ -2,14 +2,8 @@ import 'package:js/js.dart';
 import 'package:js/js_util.dart' as jsutil;
 import 'package:js_bindings/js_bindings.dart' as html;
 
-@JS()
-external void getMidiInputs(Function f);
-
-@JS()
-external void getMidiMaplike(Function f);
-
 void showInputs(dynamic a, dynamic b, dynamic c) {
-  print('got args from forEach: [$a] [$b] [$c]');
+  print('got args from forEach: |$a| |$b| |$c|');
   final inp = a as html.MIDIInput;
   print('input name: ${inp.name}');
 }
@@ -23,4 +17,7 @@ void main() async {
   final inputs = access.inputs;
 
   jsutil.callMethod(inputs, 'forEach', [allowInterop(showInputs)]);
+  inputs.forEach((p0, p1) {
+    print('got args from DART forEach: |$p0| |$p1|');
+  });
 }
