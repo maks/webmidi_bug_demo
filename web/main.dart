77642dbe669fce997e5f2d7e6ad988d75a82bee6
Maksim Lin <maks@manichord.com>
2022-02-08 20:56:12 +1100

    remove use of dart:html

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:js/js.dart';
 import 'package:js/js_util.dart' as jsutil;
 import 'package:js_bindings/js_bindings.dart' as html;
@@ -12,12 +10,12 @@ external void getMidiMaplike(Function f);
 
 void showInputs(dynamic a, dynamic b, dynamic c) {
   print('got args from forEach: [$a] [$b] [$c]');
-  final inp = a as MidiInput;
+  final inp = a as html.MIDIInput;
   print('input name: ${inp.name}');
 }
 
 void main() async {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+  //querySelector('#output')?.text = 'Your Dart app is running.';
 
   final access = await html.window.navigator
       .requestMIDIAccess(html.MIDIOptions(sysex: true, software: false));
